9af0790124567065fd02ccda0bc29d7c5a663a0e
prathuedu-create <prathuedu@gmail.com>
2026-03-04 15:44:38 +0530

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,10 +1,29 @@
 package com.example;
+
 import org.junit.Test;
 import static org.junit.Assert.*;
+
 public class AppTest {
- @Test
- public void testAdd() {
- App app = new App();
- assertEquals(5, app.add(2, 3));
- }
+
+    App calc = new App();
+
+    @Test
+    public void testAdd() {
+        assertEquals(15, calc.add(10, 5));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(5, calc.subtract(10, 5));
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(50, calc.multiply(10, 5));
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(2, calc.divide(10, 5));
+    }
 }
