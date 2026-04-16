05a1b393e5ecaa5b86fdc2475664becbe271de4b
prathuedu-create <prathuedu@gmail.com>
2026-04-16 14:19:23 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,26 +1,57 @@
 package com.example;
+import java.util.Scanner;
+
 public class App {
-    public int add(int a, int b) {
-        return a + b;
-    }
-    public int subtract(int a, int b) {
-        return a - b;
-    }
-    public int multiply(int a, int b) {
-        return a * b;
-    }
-    public int divide(int a, int b) {
-        if (b == 0) {
-            throw new ArithmeticException("Cannot divide by zero");
+    public static int calculateFine(int days) {
+        int allowedDays = 7;
+        int finePerDay = 5;
+
+        if (days > allowedDays) {
+            return (days - allowedDays) * finePerDay;
+        } else {
+            return 0;
         }
-        return a / b;
     }
+
     public static void main(String[] args) {
-        App calc = new App();
-        System.out.println("Addition: " + calc.add(10, 5));
-        System.out.println("Subtraction: " + calc.subtract(10, 5));
-        System.out.println("Multiplication: " + calc.multiply(10, 5));
-        System.out.println("Division: " + calc.divide(10, 5));
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter User Name:");
+        String name = sc.nextLine();
+
+        System.out.println("Enter User ID:");
+        String id = sc.nextLine();
+
+        System.out.println("Enter Number of Books Issued:");
+        int books = sc.nextInt();
+
+        String[] bookNames = new String[books];
+        sc.nextLine(); // consume newline
+
+        for (int i = 0; i < books; i++) {
+            System.out.println("Enter Book " + (i + 1) + " Name:");
+            bookNames[i] = sc.nextLine();
+        }
+
+        System.out.println("Enter Number of Days Borrowed:");
+        int days = sc.nextInt();
+
+        int fine = calculateFine(days);
+
+      
+        System.out.println("\n--- User Details ---");
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
+
+        System.out.println("\nBooks Issued:");
+        for (int i = 0; i < books; i++) {
+            System.out.println("- " + bookNames[i]);
+        }
+
+        System.out.println("\nDays Borrowed: " + days);
+        System.out.println("Total Fine: ₹" + fine);
+
+        sc.close();
     }
 }
-
